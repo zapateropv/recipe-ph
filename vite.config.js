780597226d0fc0e34a2c04d49f8cc544366d154f
zapateropv <166297524+zapateropv@users.noreply.github.com>
2025-07-17 14:56:03 +0800

    fix again

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE-BASE-PATH || "/recipe-ph"
+  base: process.env.VITE_BASE_PATH || "/recipe-ph"
 })
